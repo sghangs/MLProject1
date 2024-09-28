53067faddaaf10fcb5765fbcd9ca70142c9e437a
Sunny <sunnyghangas098@gmail.com>
2024-09-29 00:08:39 +0530

    Prediction pipeline

--- src/components/data_training.py
+++ src/components/data_training.py
@@ -43,7 +43,41 @@ class ModelTrainer:
                 "Adaboost Regressor": AdaBoostRegressor(),
             }
 
-            model_report=evaluate_model(X_train,y_train,X_test,y_test,models)
+            params={
+                "Decision Tree":{
+                    "criterion":['squared_error','friedman_mse','absolute_error','poisson'],
+                    "splitter":['best','random'],
+                    "max_features":['sqrt','log2']
+                },
+                "Random Forest":{
+                   "criterion":['squared_error','friedman_mse','absolute_error','poisson'], 
+                   "max_features":['sqrt','log2'],
+                   "n_estimators":[8,16,32,64,128]
+                },
+                "Gradient Boosting":{
+                    "loss":['squared_loss','huber','absolute_error','quantile'],
+                    "learning_rate":[0.1,0.01,0.05,0.001],
+                    "subsample":[0.6,0.7,0.75,0.8,0.85,0.9],
+                    "criterion":['squared_error','friedman_mse'],
+                    "n_estimators":[8,16,32,64,128]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    "n_neighbors":[5,7,9,11],
+                    "weight":['uniform','distance'],
+                    "algorithm":['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    "learning_rate":[0.1,0.01,0.05,0.001],
+                    "n_estimators":[8,16,32,64,128]
+                },
+                "Adaboost Regressor":{
+                    "learning_rate":[0.1,0.01,0.05,0.001],
+                    "n_estimators":[8,16,32,64,128]
+                }
+            }
+
+            model_report=evaluate_model(X_train,y_train,X_test,y_test,models,params)
 
             # To get the best model from dict
             best_model_score=max(sorted(model_report.values()))
